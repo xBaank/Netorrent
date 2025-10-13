fa10451c7dc3dbc588cdb8b0afb00570e3ee2c18
xBaank <xbaank@users.noreply.github.com>
2025-10-13 20:45:10 +0200

    WIP

---
+++ Netorrent/Torrent/FileStructure/Info.cs
@@ -0,0 +1,22 @@
+﻿namespace Netorrent.Torrent.FileStructure;
+
+public enum InfoType
+{
+    Single,
+    Multiple,
+}
+
+public record Info(
+    long PieceLength,
+    string Pieces,
+    long Private,
+    InfoType Type,
+    string Name,
+    //Single file mode
+    long? Length = null,
+    string? Md5sum = null,
+    //Multiple file mode
+    List<InfoFile>? Files = null
+);
+
+public record InfoFile(long Length, List<string> Path, string? Md5sum = null);
---
+++ Netorrent/Torrent/FileStructure/MetaInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace Netorrent.Torrent.FileStructure;
+
+public record MetaInfo(
+    Info Info,
+    string Announce,
+    List<string>? AnnounceList = null,
+    long? CreationDate = null,
+    string? Comment = null,
+    string? CreatedBy = null,
+    string? Encoding = null
+);
