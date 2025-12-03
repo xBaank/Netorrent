94ebe38a154d561ae960e537f575954ea15c6f30
xBaank <xbaank@users.noreply.github.com>
2025-12-03 22:01:34 +0100

    a

--- Netorrent/Other/RentedArray.cs
+++ Netorrent/Other/RentedArray.cs
@@ -32,8 +32,8 @@ internal class RentedArray<T>(T[] array, int length, int start = 0) : IDisposabl
     {
         if (!_disposed)
         {
-            ArrayPool<T>.Shared.Return(array);
             _disposed = true;
+            ArrayPool<T>.Shared.Return(array);
             GC.SuppressFinalize(this);
         }
     }
