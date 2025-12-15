994db8985ce3b24b37eb555cc70657adde9022d6
xBaank <xbaank@users.noreply.github.com>
2025-12-15 20:00:29 +0100

    wip

--- Netorrent.Tests/Tracker/TrackerTests.cs
+++ Netorrent.Tests/Tracker/TrackerTests.cs
@@ -1,5 +1,7 @@
-﻿using System.Net;
+﻿using System.Linq;
+using System.Net;
 using System.Threading.Channels;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 using Netorrent.Extensions;
 using Netorrent.Tests.Fakes;
@@ -13,8 +15,14 @@ namespace Netorrent.Tests.Tracker;
 [Timeout(10_000)]
 public class TrackerTests
 {
-    [Test]
-    public async Task Should_get_peers_from_udp_tracker(CancellationToken cancellationToken)
+    private record TestContext(
+        IPEndPoint[] Ips,
+        Channel<IPEndPoint> Channel,
+        ILogger Logger,
+        TimeSpan Interval
+    );
+
+    private static TestContext CreateDefaultContext()
     {
         var interval = 1.Seconds;
         IPEndPoint[] ips =
@@ -24,36 +32,61 @@ public class TrackerTests
             new IPEndPoint(IPAddress.Parse("::1"), 6881),
             new IPEndPoint(IPAddress.Parse("2001:db8::1"), 6882),
         ];
-        var channel = Channel.CreateUnbounded<IPEndPoint>();
-        var logger = NullLogger.Instance;
+
+        return new TestContext(
+            Ips: ips,
+            Channel: Channel.CreateUnbounded<IPEndPoint>(),
+            Logger: NullLogger.Instance,
+            Interval: interval
+        );
+    }
+
+    private static Task<IPEndPoint[]> ReadTakeAsync(
+        ChannelReader<IPEndPoint> reader,
+        int count,
+        CancellationToken ct
+    )
+    {
+        return reader.ReadAllAsync(ct).Take(count).ToArrayAsync(cancellationToken: ct).AsTask();
+    }
+
+    [Test]
+    public async Task Should_get_peers_from_udp_tracker(CancellationToken cancellationToken)
+    {
+        var ctx = CreateDefaultContext();
+
         await using var udptrackerManager = new FakeUdpTrackerTransactionManager(
-            ips,
-            interval,
+            ctx.Ips,
+            ctx.Interval,
             false
         );
+
         await using var udptracker = new UdpTracker(
             udptrackerManager,
             1,
             new(3),
             new(),
-            channel.Writer,
+            ctx.Channel.Writer,
             [1, 2, 3],
             "null",
             new(IPAddress.Loopback, 1),
-            logger,
+            ctx.Logger,
             null
         );
 
         using var cts = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
         var trackerTask = udptracker.StartAsync(cts.Token).AsTask();
-        var ipendpoints = await channel
-            .Reader.ReadAllAsync(cancellationToken)
-            .Take(ips.Length * 2)
-            .ToArrayAsync(cancellationToken);
+
+        var ipendpoints = await ReadTakeAsync(
+            ctx.Channel.Reader,
+            ctx.Ips.Length * 2,
+            cancellationToken
+        );
+
         cts.Cancel();
 
-        IPEndPoint[] resultIps = [.. ips, .. ips];
-        ipendpoints.Length.ShouldBe(ips.Length * 2);
+        IPEndPoint[] resultIps = ctx.Ips.Concat(ctx.Ips).ToArray();
+        ipendpoints.Length.ShouldBe(ctx.Ips.Length * 2);
         ipendpoints.ShouldBeEquivalentTo(resultIps);
         await trackerTask.ShouldThrowAsync<OperationCanceledException>();
     }
@@ -61,39 +94,33 @@ public class TrackerTests
     [Test]
     public async Task Should_get_peers_from_http_tracker(CancellationToken cancellationToken)
     {
-        var interval = 1.Seconds;
-        IPEndPoint[] ips =
-        [
-            new IPEndPoint(IPAddress.Parse("127.0.0.1"), 6881),
-            new IPEndPoint(IPAddress.Parse("127.0.0.2"), 6882),
-            new IPEndPoint(IPAddress.Parse("::1"), 6881),
-            new IPEndPoint(IPAddress.Parse("2001:db8::1"), 6882),
-        ];
-        var channel = Channel.CreateUnbounded<IPEndPoint>();
-        var logger = NullLogger.Instance;
-        using var httpClient = new HttpClient();
+        var ctx = CreateDefaultContext();
+
         var httpTracker = new HttpTracker(
             1,
             new Statistics.TransferStatistics(3),
-            new FakeHttpTrackerHandler(ips, interval, false),
+            new FakeHttpTrackerHandler(ctx.Ips, ctx.Interval, false),
             new(),
             [1, 2, 3],
             "null",
-            logger,
-            channel,
+            ctx.Logger,
+            ctx.Channel,
             null
         );
 
         using var cts = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
         var trackerTask = httpTracker.StartAsync(cts.Token).AsTask();
-        var ipendpoints = await channel
-            .Reader.ReadAllAsync(cancellationToken)
-            .Take(ips.Length * 2)
-            .ToArrayAsync(cancellationToken: cancellationToken);
+
+        var ipendpoints = await ReadTakeAsync(
+            ctx.Channel.Reader,
+            ctx.Ips.Length * 2,
+            cancellationToken
+        );
+
         cts.Cancel();
 
-        IPEndPoint[] resultIps = [.. ips, .. ips];
-        ipendpoints.Length.ShouldBe(ips.Length * 2);
+        IPEndPoint[] resultIps = ctx.Ips.Concat(ctx.Ips).ToArray();
+        ipendpoints.Length.ShouldBe(ctx.Ips.Length * 2);
         ipendpoints.ShouldBeEquivalentTo(resultIps);
         await trackerTask.ShouldThrowAsync<OperationCanceledException>();
     }
@@ -101,26 +128,17 @@ public class TrackerTests
     [Test]
     public async Task Should_not_get_peers_from_http_tracker(CancellationToken cancellationToken)
     {
-        var interval = 1.Seconds;
-        IPEndPoint[] ips =
-        [
-            new IPEndPoint(IPAddress.Parse("127.0.0.1"), 6881),
-            new IPEndPoint(IPAddress.Parse("127.0.0.2"), 6882),
-            new IPEndPoint(IPAddress.Parse("::1"), 6881),
-            new IPEndPoint(IPAddress.Parse("2001:db8::1"), 6882),
-        ];
-        var channel = Channel.CreateUnbounded<IPEndPoint>();
-        var logger = NullLogger.Instance;
-        using var httpClient = new HttpClient();
+        var ctx = CreateDefaultContext();
+
         var httpTracker = new HttpTracker(
             1,
             new Statistics.TransferStatistics(3),
-            new FakeHttpTrackerHandler(ips, interval, true),
+            new FakeHttpTrackerHandler(ctx.Ips, ctx.Interval, true),
             new(),
             [1, 2, 3],
             "null",
-            logger,
-            channel,
+            ctx.Logger,
+            ctx.Channel,
             null
         );
 
@@ -129,10 +147,10 @@ public class TrackerTests
 
         await Task.Delay(5.Seconds, cancellationToken);
         cts.Cancel();
-        channel.Writer.TryComplete();
+        ctx.Channel.Writer.TryComplete();
 
-        var ipendpoints = await channel
-            .Reader.ReadAllAsync(cancellationToken)
+        var ipendpoints = await ctx
+            .Channel.Reader.ReadAllAsync(cancellationToken)
             .ToArrayAsync(cancellationToken: cancellationToken)
             .AsTask();
 
@@ -143,42 +161,36 @@ public class TrackerTests
     [Test]
     public async Task Should_not_get_peers_from_udp_tracker(CancellationToken cancellationToken)
     {
-        var interval = 1.Seconds;
-        IPEndPoint[] ips =
-        [
-            new IPEndPoint(IPAddress.Parse("127.0.0.1"), 6881),
-            new IPEndPoint(IPAddress.Parse("127.0.0.2"), 6882),
-            new IPEndPoint(IPAddress.Parse("::1"), 6881),
-            new IPEndPoint(IPAddress.Parse("2001:db8::1"), 6882),
-        ];
-        var channel = Channel.CreateUnbounded<IPEndPoint>();
-        var logger = NullLogger.Instance;
+        var ctx = CreateDefaultContext();
+
         await using var udptrackerManager = new FakeUdpTrackerTransactionManager(
-            ips,
-            interval,
+            ctx.Ips,
+            ctx.Interval,
             true
         );
+
         await using var udptracker = new UdpTracker(
             udptrackerManager,
             1,
             new(3),
             new(),
-            channel.Writer,
+            ctx.Channel.Writer,
             [1, 2, 3],
             "null",
             new(IPAddress.Loopback, 1),
-            logger,
+            ctx.Logger,
             null
         );
 
         using var cts = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
         var trackerTask = udptracker.StartAsync(cts.Token).AsTask();
+
         await Task.Delay(5.Seconds, cancellationToken);
         cts.Cancel();
-        channel.Writer.TryComplete();
+        ctx.Channel.Writer.TryComplete();
 
-        var ipendpoints = await channel
-            .Reader.ReadAllAsync(cancellationToken)
+        var ipendpoints = await ctx
+            .Channel.Reader.ReadAllAsync(cancellationToken)
             .ToArrayAsync(cancellationToken: cancellationToken)
             .AsTask();
 
@@ -189,22 +201,15 @@ public class TrackerTests
     [Test]
     public async Task Should_get_peers_from_tracker_client(CancellationToken cancellationToken)
     {
-        var interval = 1.Seconds;
-        IPEndPoint[] ips =
-        [
-            new IPEndPoint(IPAddress.Parse("127.0.0.1"), 6881),
-            new IPEndPoint(IPAddress.Parse("127.0.0.2"), 6882),
-            new IPEndPoint(IPAddress.Parse("::1"), 6881),
-            new IPEndPoint(IPAddress.Parse("2001:db8::1"), 6882),
-        ];
-        var channel = Channel.CreateUnbounded<IPEndPoint>();
-        var logger = NullLogger.Instance;
+        var ctx = CreateDefaultContext();
+
         await using var udptrackerManager = new FakeUdpTrackerTransactionManager(
-            ips,
-            interval,
+            ctx.Ips,
+            ctx.Interval,
             false
         );
-        var httpTrackerHandler = new FakeHttpTrackerHandler(ips, interval, false);
+
+        var httpTrackerHandler = new FakeHttpTrackerHandler(ctx.Ips, ctx.Interval, false);
 
         await using var trackerClient = new TrackerClient(
             httpTrackerHandler,
@@ -212,7 +217,7 @@ public class TrackerTests
             1,
             new(3),
             new(),
-            channel,
+            ctx.Channel,
             [
                 "udp://localhost:1",
                 "https://localhost:2",
@@ -220,21 +225,22 @@ public class TrackerTests
                 "aaaa://localhost:4",
             ],
             [1, 2, 3],
-            logger,
+            ctx.Logger,
             null
         );
 
         using var cts = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
         var trackerTask = trackerClient.StartAsync(cts.Token);
-        var ipendpoints = await channel
-            .Reader.ReadAllAsync(cancellationToken)
-            .Take(ips.Length * 4) // 2 for udp ipv4 and ipv6 and 2 for http and https
-            .ToArrayAsync(cancellationToken: cancellationToken);
+        var ipendpoints = await ReadTakeAsync(
+            ctx.Channel.Reader,
+            ctx.Ips.Length * 4,
+            cancellationToken
+        );
 
         cts.Cancel();
 
-        IPEndPoint[] resultIps = [.. ips, .. ips, .. ips, .. ips];
-        ipendpoints.Length.ShouldBe(ips.Length * 4);
+        IPEndPoint[] resultIps = ctx.Ips.Concat(ctx.Ips).Concat(ctx.Ips).Concat(ctx.Ips).ToArray();
+        ipendpoints.Length.ShouldBe(ctx.Ips.Length * 4);
         ipendpoints.ShouldBeEquivalentTo(resultIps);
         await trackerTask.ShouldThrowAsync<OperationCanceledException>();
     }
