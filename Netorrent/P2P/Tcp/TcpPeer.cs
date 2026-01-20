f6b344718fb04e9c8dc546242ddcd31b4245cf25
xBaank <xbaank@users.noreply.github.com>
2026-01-20 18:37:32 +0100

    wip

--- Netorrent/P2P/Tcp/TcpPeer.cs
+++ Netorrent/P2P/Tcp/TcpPeer.cs
@@ -29,7 +29,7 @@ internal class TcpPeer(
         await tcpClient.ConnectAsync(iPEndPoint, cts.Token).ConfigureAwait(false);
 
         var handshake = await Handshake
-            .PerformHandshakeAsync(tcpClient.GetStream(), infoHash, peerId, cts.Token)
+            .PerformHandshakeAsync(tcpClient.GetStream(), infoHash, peerId, cancellationToken)
             .ConfigureAwait(false);
 
         return _tcpMessageStream = tcpClient.GetMessageStream(handshake);
