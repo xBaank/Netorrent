efe68bdd5112b03a1a5830faa7d59b45ae9236d9
xBaank <xbaank@users.noreply.github.com>
2026-02-14 23:45:59 +0100

    wip

--- Netorrent.Tests/P2P/RequestSchedulerTests.cs
+++ Netorrent.Tests/P2P/RequestSchedulerTests.cs
@@ -128,7 +128,6 @@ public class RequestSchedulerTests
     }
 
     [Test]
-    // [Skip("Not fully implemented")]
     public async Task Should_Request_All_Block_From_Peers_On_Endgame(
         CancellationToken cancellationToken
     )
