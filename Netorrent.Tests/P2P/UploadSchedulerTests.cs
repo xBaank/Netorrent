41c76511e93026c4557664c61c386033fc12be36
xBaank <xbaank@users.noreply.github.com>
2026-02-16 15:33:05 +0100

    Cleanup `UploadSchedulerTests`

--- Netorrent.Tests/P2P/UploadSchedulerTests.cs
+++ Netorrent.Tests/P2P/UploadSchedulerTests.cs
@@ -1,3 +1,4 @@
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 using Netorrent.P2P;
 using Netorrent.P2P.Messages;
@@ -11,40 +12,48 @@ namespace Netorrent.Tests.P2P;
 [Timeout(10_000)]
 public class UploadSchedulerTests
 {
-    //TODO use matrix to create multiple peers
-    [Test]
-    public async Task Should_Upload_Block_To_Peer(CancellationToken cancellationToken)
+    const int PieceCount = 5;
+    const int BlockSize = 1024 * 4;
+
+    static ILogger Logger => NullLogger.Instance;
+
+    static async Task<(
+        FakePeerConnection Peer,
+        UploadScheduler Scheduler
+    )> CreateUploadSchedulerAsync(CancellationToken cancellationToken)
     {
-        var logger = NullLogger.Instance;
-        var bitfield = new Bitfield(5, true);
-        var leecherBitfield = new Bitfield(5, false);
-        await using var leecherPeerConnection = new FakePeerConnection(
-            bitfield,
-            leecherBitfield,
-            1024 * 4
-        );
-        leecherPeerConnection.PeerInterested.Value = true;
-        await using var uploadScheduler = new UploadScheduler(
-            new Dictionary<PeerEndpoint, IPeerConnection>()
-            {
-                [leecherPeerConnection.PeerEndpoint] = leecherPeerConnection,
-            },
+        var bitfield = new Bitfield(PieceCount, true);
+        var leecherBitfield = new Bitfield(PieceCount, false);
+        var peer = new FakePeerConnection(bitfield, leecherBitfield, BlockSize);
+        peer.PeerInterested.Value = true;
+
+        var scheduler = new UploadScheduler(
+            new Dictionary<PeerEndpoint, IPeerConnection> { [peer.PeerEndpoint] = peer },
             new FakePieceStorage(),
             bitfield,
             new DataStatistics(10),
-            logger
+            Logger
         );
+
+        return (peer, scheduler);
+    }
+
+    [Test]
+    public async Task Should_Upload_Block_To_Peer(CancellationToken cancellationToken)
+    {
+        var (peer, scheduler) = await CreateUploadSchedulerAsync(cancellationToken);
+        await using var __ = scheduler;
+
         var requestBlock = new RequestBlock(0, 0, 0);
-        var amChokingTask = leecherPeerConnection.AmChoking.FirstAsync(
-            i => i == false,
-            cancellationToken
-        );
-        var blockTask = leecherPeerConnection.SentBlocks.FirstAsync(cancellationToken);
-        requestBlock.RequestedFrom.Add(leecherPeerConnection);
-        var uploadTask = uploadScheduler.StartAsync(cancellationToken);
+        var amChokingTask = peer.AmChoking.FirstAsync(i => i == false, cancellationToken);
+        var blockTask = peer.SentBlocks.FirstAsync(cancellationToken);
+        requestBlock.RequestedFrom.Add(peer);
+        _ = scheduler.StartAsync(cancellationToken);
+
         var chokeState = await amChokingTask;
-        await uploadScheduler.AddRequestAsync(requestBlock, cancellationToken);
+        await scheduler.AddRequestAsync(requestBlock, cancellationToken);
         var block = await blockTask;
+
         block.Index.ShouldBe(0);
         block.Begin.ShouldBe(0);
         block.Payload.Length.ShouldBe(0);
@@ -54,34 +63,21 @@ public class UploadSchedulerTests
     [Test]
     public async Task Should_Cancel_Block_To_Peer(CancellationToken cancellationToken)
     {
-        var logger = NullLogger.Instance;
-        var bitfield = new Bitfield(5, true);
-        var leecherBitfield = new Bitfield(5, false);
-        var leecherPeerConnection = new FakePeerConnection(bitfield, leecherBitfield, 1024 * 4);
-        leecherPeerConnection.PeerInterested.Value = true;
-        await using var uploadScheduler = new UploadScheduler(
-            new Dictionary<PeerEndpoint, IPeerConnection>()
-            {
-                [leecherPeerConnection.PeerEndpoint] = leecherPeerConnection,
-            },
-            new FakePieceStorage(),
-            bitfield,
-            new DataStatistics(10),
-            logger
-        );
+        var (peer, scheduler) = await CreateUploadSchedulerAsync(cancellationToken);
+        await using var __ = scheduler;
+
         var requestBlock = new RequestBlock(0, 0, 0);
-        var amChokingTask = leecherPeerConnection.AmChoking.FirstAsync(
-            i => i == false,
-            cancellationToken
-        );
-        var isEmptyTask = leecherPeerConnection.SentBlocks.IsEmptyAsync(cancellationToken);
-        requestBlock.RequestedFrom.Add(leecherPeerConnection);
-        var uploadTask = uploadScheduler.StartAsync(cancellationToken);
+        var amChokingTask = peer.AmChoking.FirstAsync(i => i == false, cancellationToken);
+        var isEmptyTask = peer.SentBlocks.IsEmptyAsync(cancellationToken);
+        requestBlock.RequestedFrom.Add(peer);
+        _ = scheduler.StartAsync(cancellationToken);
+
         var chokeState = await amChokingTask;
-        await uploadScheduler.AddRequestAsync(requestBlock, cancellationToken);
-        uploadScheduler.CancelRequest(requestBlock);
+        await scheduler.AddRequestAsync(requestBlock, cancellationToken);
+        scheduler.CancelRequest(requestBlock);
         await Task.Delay(1000, cancellationToken);
-        await leecherPeerConnection.DisposeAsync();
+        await peer.DisposeAsync();
+
         (await isEmptyTask).ShouldBe(true);
         chokeState.ShouldBe(false);
     }
