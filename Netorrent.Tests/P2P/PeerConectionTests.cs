64c14c354f95c0f1c9f266b5a9a324eed8b111e2
xBaank <xbaank@users.noreply.github.com>
2026-02-16 15:32:47 +0100

    Cleanup `PeerConnectionTests`

--- Netorrent.Tests/P2P/PeerConectionTests.cs
+++ Netorrent.Tests/P2P/PeerConectionTests.cs
@@ -1,4 +1,4 @@
-﻿using System.Net;
+using System.Net;
 using System.Threading.Channels;
 using Netorrent.P2P;
 using Netorrent.P2P.Download;
@@ -12,105 +12,28 @@ namespace Netorrent.Tests.P2P;
 [Timeout(5_000)]
 public class PeerConectionTests
 {
-    [Test]
-    public async Task Should_Receive_Unchoke(CancellationToken cancellationToken)
+    const int Port = 6881;
+
+    static IPEndPoint Endpoint => new(IPAddress.Loopback, Port);
+    static PeerId OtherPeerId => new();
+
+    static async Task<(
+        PeerConnection PeerConnection,
+        Channel<Message> Incoming,
+        Channel<Message> Outgoing
+    )> CreateTestContextAsync(bool bitfieldFull)
     {
-        var ipEndpoint = new IPEndPoint(IPAddress.Loopback, 6881);
-        var otherPeerId = new PeerId();
-        var incommingMessages = Channel.CreateUnbounded<Message>();
-        var outgoingMessages = Channel.CreateUnbounded<Message>();
-        var bitfield = new Bitfield(10, true);
-        await using var peerConnection = CreatePeerConnection(
-            ipEndpoint,
-            otherPeerId,
-            incommingMessages,
-            outgoingMessages,
+        var incoming = Channel.CreateUnbounded<Message>();
+        var outgoing = Channel.CreateUnbounded<Message>();
+        var bitfield = new Bitfield(10, bitfieldFull);
+        var peerConnection = CreatePeerConnection(
+            Endpoint,
+            OtherPeerId,
+            incoming,
+            outgoing,
             bitfield
         );
-
-        _ = peerConnection.StartAsync(cancellationToken);
-        var statesTask = peerConnection.PeerChoking.Take(2).ToListAsync(cancellationToken);
-        await incommingMessages.Writer.WriteAsync(Message.CreateUnchoke(), cancellationToken);
-        var states = await statesTask;
-
-        states.First().ShouldBeTrue();
-        states.Last().ShouldBeFalse();
-    }
-
-    [Test]
-    public async Task Should_Send_Unchoke(CancellationToken cancellationToken)
-    {
-        var ipEndpoint = new IPEndPoint(IPAddress.Loopback, 6881);
-        var otherPeerId = new PeerId();
-        var incommingMessages = Channel.CreateUnbounded<Message>();
-        var outgoingMessages = Channel.CreateUnbounded<Message>();
-        var bitfield = new Bitfield(10, true);
-        await using var peerConnection = CreatePeerConnection(
-            ipEndpoint,
-            otherPeerId,
-            incommingMessages,
-            outgoingMessages,
-            bitfield
-        );
-
-        _ = peerConnection.StartAsync(cancellationToken);
-        var statesTask = peerConnection.AmChoking.Take(2).ToListAsync(cancellationToken);
-
-        peerConnection.Unchoke();
-        var states = await statesTask;
-
-        states.First().ShouldBeTrue();
-        states.Last().ShouldBeFalse();
-    }
-
-    [Test]
-    public async Task Should_Receive_Interested(CancellationToken cancellationToken)
-    {
-        var ipEndpoint = new IPEndPoint(IPAddress.Loopback, 6881);
-        var otherPeerId = new PeerId();
-        var incommingMessages = Channel.CreateUnbounded<Message>();
-        var outgoingMessages = Channel.CreateUnbounded<Message>();
-        var bitfield = new Bitfield(10, true);
-        await using var peerConnection = CreatePeerConnection(
-            ipEndpoint,
-            otherPeerId,
-            incommingMessages,
-            outgoingMessages,
-            bitfield
-        );
-
-        _ = peerConnection.StartAsync(cancellationToken);
-        var statesTask = peerConnection.PeerInterested.Take(2).ToListAsync(cancellationToken);
-        await incommingMessages.Writer.WriteAsync(Message.CreateInterested(), cancellationToken);
-        var states = await statesTask;
-
-        states.First().ShouldBeFalse();
-        states.Last().ShouldBeTrue();
-    }
-
-    [Test]
-    public async Task Should_Send_Interested(CancellationToken cancellationToken)
-    {
-        var ipEndpoint = new IPEndPoint(IPAddress.Loopback, 6881);
-        var otherPeerId = new PeerId();
-        var incommingMessages = Channel.CreateUnbounded<Message>();
-        var outgoingMessages = Channel.CreateUnbounded<Message>();
-        var bitfield = new Bitfield(10, false);
-        await using var peerConnection = CreatePeerConnection(
-            ipEndpoint,
-            otherPeerId,
-            incommingMessages,
-            outgoingMessages,
-            bitfield
-        );
-
-        _ = peerConnection.StartAsync(cancellationToken);
-        var statesTask = peerConnection.AmInterested.Take(2).ToListAsync(cancellationToken);
-        await incommingMessages.Writer.WriteAsync(Message.CreateHave(5), cancellationToken);
-        var states = await statesTask;
-
-        states.First().ShouldBeFalse();
-        states.Last().ShouldBeTrue();
+        return (peerConnection, incoming, outgoing);
     }
 
     private static PeerConnection CreatePeerConnection(
@@ -129,4 +52,65 @@ public class PeerConectionTests
             new PeerRequestWindow(16 * 1024),
             new PiecePicker(bitfield, 16 * 1024, 256 * 1024, 256 * 1024)
         );
+
+    [Test]
+    public async Task Should_Receive_Unchoke(CancellationToken cancellationToken)
+    {
+        var (peerConnection, incoming, _) = await CreateTestContextAsync(true);
+        await using var __ = peerConnection;
+
+        _ = peerConnection.StartAsync(cancellationToken);
+        var statesTask = peerConnection.PeerChoking.Take(2).ToListAsync(cancellationToken);
+        await incoming.Writer.WriteAsync(Message.CreateUnchoke(), cancellationToken);
+        var states = await statesTask;
+
+        states.First().ShouldBeTrue();
+        states.Last().ShouldBeFalse();
+    }
+
+    [Test]
+    public async Task Should_Send_Unchoke(CancellationToken cancellationToken)
+    {
+        var (peerConnection, _, _) = await CreateTestContextAsync(true);
+        await using var __ = peerConnection;
+
+        _ = peerConnection.StartAsync(cancellationToken);
+        var statesTask = peerConnection.AmChoking.Take(2).ToListAsync(cancellationToken);
+
+        peerConnection.Unchoke();
+        var states = await statesTask;
+
+        states.First().ShouldBeTrue();
+        states.Last().ShouldBeFalse();
+    }
+
+    [Test]
+    public async Task Should_Receive_Interested(CancellationToken cancellationToken)
+    {
+        var (peerConnection, incoming, _) = await CreateTestContextAsync(true);
+        await using var __ = peerConnection;
+
+        _ = peerConnection.StartAsync(cancellationToken);
+        var statesTask = peerConnection.PeerInterested.Take(2).ToListAsync(cancellationToken);
+        await incoming.Writer.WriteAsync(Message.CreateInterested(), cancellationToken);
+        var states = await statesTask;
+
+        states.First().ShouldBeFalse();
+        states.Last().ShouldBeTrue();
+    }
+
+    [Test]
+    public async Task Should_Send_Interested(CancellationToken cancellationToken)
+    {
+        var (peerConnection, incoming, _) = await CreateTestContextAsync(false);
+        await using var __ = peerConnection;
+
+        _ = peerConnection.StartAsync(cancellationToken);
+        var statesTask = peerConnection.AmInterested.Take(2).ToListAsync(cancellationToken);
+        await incoming.Writer.WriteAsync(Message.CreateHave(5), cancellationToken);
+        var states = await statesTask;
+
+        states.First().ShouldBeFalse();
+        states.Last().ShouldBeTrue();
+    }
 }
